56671ee8296a18b166ab0fd6b051154517090ba4
sahilmarka <m.sahilkumar1234@gmail.com>
2021-07-10 18:08:36 +0530

    complete routes and add restriction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,8 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 $(document).on('turbolinks:load', function() {
-    $('.ui.dropdown')
-    .dropdown()
-  ;
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
